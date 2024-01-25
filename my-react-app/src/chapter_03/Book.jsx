9cb4dde1d8261ab043a478e39aeede9170bc7a42
Jiyeon-Kang <kang158632@gmail.com>
2024-01-25 18:07:24 +0900

    inflearn chapter_03:practice

---
+++ my-react-app/src/chapter_03/Book.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Book(props) {
+
+    return (
+       <div>
+           <h1>{`this book's name is ${props.name}`}</h1>
+           <h2>{`this book's is consisted of ${props.name}page`}</h2>
+       </div>
+    )
+}
+
+export default Book;
---
+++ my-react-app/src/chapter_03/Library.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Book from "./Book"
+function Library(props) {
+
+    return (
+        <div>
+            <Book name='first steps of python' numOfPage={300}/>
+            <Book name='first steps of AWS' numOfPage={400}/>
+            <Book name='first steps of react' numOfPage={500}/>
+        </div>
+    )
+}
+
+export default Library;
