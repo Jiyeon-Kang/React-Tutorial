c1c00c82ef753be3c7b02d68809ab4486a303f5f
Jiyeon-Kang <kang158632@gmail.com>
2024-02-05 18:15:38 +0900

    inflearn chapter_04:practice

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App.jsx'
 import './index.css'
-import Library from "./chapter_03/Library.jsx";
+// import Library from "./chapter_03/Library.jsx";
+import Clock from "./chapter_04/Clock.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Library />
-  </React.StrictMode>,
-)
+const root=ReactDOM.createRoot(document.getElementById('root'));
+    setInterval(()=>{
+            root.render(
+                <React.StrictMode>
+                    <Clock />
+                </React.StrictMode>
+            );
+        }, 1000);
