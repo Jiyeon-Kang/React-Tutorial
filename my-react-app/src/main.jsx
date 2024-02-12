287d41bbf786f3f6e96fd7751fd4f1e18cb0101a
Jiyeon-Kang <kang158632@gmail.com>
2024-02-12 19:30:59 +0900

    inflearn chapter_07:practice

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -5,12 +5,13 @@ import './index.css'
 // import Library from "./chapter_03/Library.jsx";
 // import Clock from "./chapter_04/Clock.jsx";
 // import CommentList from "./Chapter_05/CommentList.jsx";
-import NotificationList from "./chapter_06/NotificationList.jsx";
+// import NotificationList from "./chapter_06/NotificationList.jsx";
+import Accommodate from "./chapter_07/Accommodate.jsx";
 
 const root=ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-        <NotificationList />
+        <Accommodate />
     </React.StrictMode>
 );
