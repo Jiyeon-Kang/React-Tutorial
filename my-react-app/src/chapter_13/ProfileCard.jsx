39328e2be4eee5f3b3506dddaac79006472546eb
Jiyeon-Kang <kang158632@gmail.com>
2024-02-20 23:46:37 +0900

    inflearn chapter_13:Composition vs Inheritance

---
+++ my-react-app/src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "./Card";
+
+function ProfileCard(props) {
+    return (
+        <Card title="Inje Lee" backgroundColor="#4ea04e">
+            <p>Hi, I&apos;am Jiyeon</p>
+            <p>I&apos;am programming using React</p>
+        </Card>
+    );
+}
+
+export default ProfileCard;
--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -11,12 +11,13 @@ import './index.css'
 // import LandingPage from "./chapter_09/LandingPage.jsx";
 // import AttendeanceBook from "./chapter_10/AttendeanceBook.jsx";
 // import SignUp from "./chapter_11/SignUp.jsx";
-import Calculator from "./chapter_12/Calculator.jsx";
+// import Calculator from "./chapter_12/Calculator.jsx";
+import ProfileCard from "./chapter_13/ProfileCard.jsx";
 
 const root=ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-        <Calculator/>
+        <ProfileCard/>
     </React.StrictMode>
 );
